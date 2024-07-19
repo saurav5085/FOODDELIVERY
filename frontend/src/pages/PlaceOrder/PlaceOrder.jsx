036c06315cfd877236db9f0ac8283d6842681a80
saurav5085 <153294397+saurav5085@users.noreply.github.com>
2024-07-19 18:11:08 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -85,7 +85,7 @@ const PlaceOrder = () => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p>₹{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
@@ -95,7 +95,7 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
             <b>Total</b>
-            <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+            <b>₹{getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
           </div>
           </div>
           <button type='submit'>PROCEED TO PAYMENT</button>
