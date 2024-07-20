4749ae1d5696873245254b6fca86bbde25ec2566
saurav5085 <153294397+saurav5085@users.noreply.github.com>
2024-07-20 12:14:26 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -27,9 +27,9 @@ const Cart = () => {
                 >
                   <img src={url+"/images/"+item.image} alt="" />
                   <p>{item.name}</p>
-                  <p>${item.price}</p>
+                  <p>₹{item.price}</p>
                   <p>{cartItems[item._id]}</p>
-                  <p>${item.price * cartItems[item._id]}</p>
+                  <p>₹{item.price * cartItems[item._id]}</p>
                   <p onClick={()=>removeFromCart(item._id)}className="cross">x</p>
                 </div>
                 <hr />
@@ -44,17 +44,17 @@ const Cart = () => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p>₹{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery fee</p>
-              <p>${getTotalCartAmount()===0?0:2}</p>
+              <p>₹{getTotalCartAmount()===0?0:20}</p>
             </div>
             <hr />
             <div className="cart-total-details">
             <b>Total</b>
-            <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+            <b>₹{getTotalCartAmount()===0?0:getTotalCartAmount()+20}</b>
           </div>
           </div>
           <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
